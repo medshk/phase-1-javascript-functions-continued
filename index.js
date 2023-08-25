75eb000e7b8c02ac9b709238bac70d9c3a1e92ce
med <mohammed.bennaceur98@gmail.com>
2023-08-25 14:13:27 +0100

    here we go succes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+const saturdayFun = (activity = "roller-skate") =>
+  "This Saturday, I want to " + activity + "!";
+
+const mondayWork = (destination = "go to the office") =>
+  "This Monday, I will " + destination + ".";
+
+const wrapAdjective = (percentage) => (text) =>
+  "You are " + percentage + text + percentage + "!";
